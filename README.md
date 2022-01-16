49f7894c68b13ca174011d88a8b5dd477067e41a
StevenVallarsa <orionsjv@gmail.com>
2022-01-16 10:21:58 -0500

    Added README

---
+++ README.md
@@ -0,0 +1,13 @@
+# THEY SAY IT'S YOUR BIRTHDAY!
+#### Well, there's only a 1 in 365 (or 366 for leap years) of me having the same birthday too, yeah!
+
+### A simple program to practice DateTime in Java
+- Get user's birthday
+- Parse date
+- FIND:
+  -  day of the week user was born
+  - day of the week it falls/fell this year
+  - today's date and how many days until user's birthday
+  - how many years old they'll be on the user's next birthday
+- Ask if user wants to try a different date
+
--- src/main/java/com/sv/birthdaycalculator2022/App.java
+++ src/main/java/com/sv/birthdaycalculator2022/App.java
@@ -1,26 +1,20 @@
-
-
 package com.sv.birthdaycalculator2022;
 
 /**
  *
- * @author: Steven Vallarsa
- *   email: stevenvallarsa@gmail.com
- *    date: 2022-01-16
+ * @author: Steven Vallarsa email: stevenvallarsa@gmail.com date: 2022-01-16
  * purpose: A simple program to practice working with DateTime
  */
 public class App {
-    
+
     public static void main(String[] args) {
-        
+
         boolean isLooping = true;
-        
+
         while (isLooping) {
-        
+            
+        }
+
     }
 
-    
-    
-}
-    
 }
